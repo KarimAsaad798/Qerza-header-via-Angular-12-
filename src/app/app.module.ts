a8ff3be475676f2fc30c71fdc754332fb8455c86
Karim Asaad <KarimAsaad798/github.com>
2022-01-02 00:37:06 +0200

    remaining components uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { FeaturedCompaniesComponent } from './featured-companies/featured-companies.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    JobsComponent,
+    FeaturedCompaniesComponent
   ],
   imports: [
     BrowserModule,
